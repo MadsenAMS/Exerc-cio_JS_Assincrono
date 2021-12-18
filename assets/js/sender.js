4cde13c1b7b1065435b7d2b186e8a639dbf0b17f
Andre Madsen <83783164+MadsenAMS@users.noreply.github.com>
2021-12-18 10:13:18 -0300

    Minor url correction

--- assets/js/sender.js
+++ assets/js/sender.js
@@ -1,5 +1,5 @@
 function sendText(){
     inputText = document.getElementById("TextToSend").value;
-    url = `../../api/api.html?text=${inputText}`;
+    url = `api/api.html?text=${inputText}`;
     window.open(url, "_blank");
 }
